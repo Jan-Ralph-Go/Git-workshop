cc727fbbb6bd498fb7f3f02bfadcbd4d75d21a87
Malcolm-Andersen-Co <malcolm_andersen_co@dlsu.edu.ph>
2021-04-17 11:29:54 +0800

    Added the Sum file works fine

--- sum.c
+++ sum.c
@@ -6,17 +6,25 @@
  *  @param a = start value
  *  @param b = final value
  *  @return the sum, S
- * 
+ *
  * Let i = a, a + 1, a + 2, ..., b - 2, b - 1, b
  * Then S = sum of i
  * E.g.
  *    series(2,4); //  returns 9. coz, 2 + 3 + 4
  *    series(3,6); //  returns 18. coz, 3 + 4 + 5 + 6
- */ 
+ */
 int series(int a, int b){
     //Write code here
 
-    return 0; //EDIT THIS
+    int sum;
+
+    for (int i = a, i<b; i++){
+
+    sum += i;
+
+    }
+
+    return i; //EDIT THIS
 }
 
 
